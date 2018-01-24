7d678d25df9feb1b265413be34ddb0cdf2fd6f8d
Robson Tenório <robsontenorio@me.com>
2018-01-24 21:43:13 -0200

    ..

--- pages/temporada.vue
+++ pages/temporada.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="text-xs-center">
     <v-alert type="warning" :value="true">
-      Temporada inicia QUINTA-FEIRA 25 DE JANEIRO as 22h.
+      Temporada inicia QUINTA-FEIRA 25 DE JANEIRO, 22h00.
     </v-alert>
   </div>
 </template>
