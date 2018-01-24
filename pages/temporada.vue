10700a1a3a111c023086253c062c95a1b860f100
Robson Tenório <robsontenorio@me.com>
2018-01-24 21:40:22 -0200

    ..

---
+++ pages/temporada.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="text-xs-center">
+    <v-alert type="warning" :value="true">
+      Temporada inicia QUINTA-FEIRA 25 DE JANEIRO as 22h.
+    </v-alert>
+  </div>
+</template>
+
+<script>
+// import Avatar from '~/components/Avatar'
+
+export default {
+
+}
+</script>
+
+<style  scoped>
+
+</style>
